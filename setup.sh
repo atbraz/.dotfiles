fdcd8886d78f00036659fd15e09d1f65df52bcbb
atbraz <antoniomtbraz@gmail.com>
2024-07-06 15:50:01 -0300

    Added new installation command: sudo mv eza /usr/local/bin/eza

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ sudo apt install -y fzf
 wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
 sudo chmod +x eza
 sudo chown root:root eza
+sudo mv eza /usr/local/bin/eza
