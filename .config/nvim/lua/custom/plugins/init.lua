47dff47cbf41af546fbcd0aea4a2b0ed6fd5a2cf
atbraz <antonio@torreaobraz.com>
2024-07-08 11:28:49 -0300

    Updated dotfiles

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,5 +1,14 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+    { 'wakatime/vim-wakatime', lazy = false },
+    {
+        "Exafunction/codeium.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({
+            })
+        end
+    },
+}
