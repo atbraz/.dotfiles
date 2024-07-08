7506de855c2f74d0a4df7d2efc1d092a2d9001f1
atbraz <antonio@torreaobraz.com>
2024-07-08 15:35:38 -0300

    Updated dotfiles

--- .config/nvim/lua/custom/configs/init.lua
+++ .config/nvim/lua/custom/configs/init.lua
@@ -72,6 +72,7 @@ vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -121,3 +122,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+-- [[ Disable Netrw ]]
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -1,6 +1,43 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+-- return {
+--   'nvim-neo-tree/neo-tree.nvim',
+--   version = '*',
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+--     'MunifTanjim/nui.nvim',
+--   },
+--   cmd = 'Neotree float',
+--   keys = {
+--     { '\\', ':Neotree reveal float<CR>', { desc = 'NeoTree reveal' } },
+--   },
+--   opts = {
+--     position = "float",
+--     popup_border_style = "rounded",
+--     close_if_last_window = true,
+--     window = {
+--       mappings = {
+--         ['\\'] = 'close_window',
+--       },
+--       position = "float"
+--     },
+--     filesystem = {
+--       filtered_items = {
+--         visible = true,
+--         hide_dotfiles = false,
+--         hide_gitignored = false,
+--       },
+--       window = {
+--         mappings = {
+--           ['\\'] = 'close_window',
+--         },
+--         position = "float"
+--       },
+--     },
+--   },
+-- }
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -9,24 +46,47 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  cmd = 'Neotree float',
   keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '\\', ':Neotree reveal float<CR>', { desc = 'NeoTree reveal' } },
   },
   opts = {
+    position = "float",
+    popup_border_style = "rounded",
+    close_if_last_window = true,
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+      },
+      position = "float"
+    },
     filesystem = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
       },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
+        position = "float"
       },
     },
   },
+  config = function(_, opts)
+    -- Adding debug prints
+    print("Loading Neo-tree with opts: ", vim.inspect(opts))
+
+    -- Setting up Neo-tree with options
+    require('neo-tree').setup(opts)
+
+    -- Debugging Neotree state
+    vim.cmd([[
+      augroup NeoTreeDebug
+        autocmd!
+        autocmd BufWinEnter * if &ft ==# 'neo-tree' | echom "Neo-tree opened" | endif
+      augroup END
+    ]])
+  end,
 }
