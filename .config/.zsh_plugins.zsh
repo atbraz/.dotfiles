a2810109c65ed9040c495236c9c2bee588312a5a
atbraz <antonio@torreaobraz.com>
2024-08-04 17:57:54 -0300

    feat: add .zsh_plugins.zsh

---
+++ .config/.zsh_plugins.zsh
@@ -0,0 +1,10 @@
+fpath+=( /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wbingli-SLASH-zsh-wakatime )
+source /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-wbingli-SLASH-zsh-wakatime/zsh-wakatime.plugin.zsh
+fpath+=( /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
+source /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh
+fpath+=( /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages )
+source /home/antonio/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
