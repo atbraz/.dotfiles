bbfae5b1eb794bfce80b4c63bebd42763a47cfbf
atbraz <antonio@torreaobraz.com>
2024-08-31 17:57:38 -0300

    chore: add clean and smudge logs for GIT_TRACE=1

--- scripts/smudge.sh
+++ scripts/smudge.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Check if GIT_TRACE is set (which happens in verbose mode)
+if [ -n "$GIT_TRACE" ]; then
+    echo "Smudge filter running on: ${1:-<no filename provided>}" >&2
+fi
+
 sed -e "s|%%HOME%%|$HOME|g" \
     -e "s|%%GIT_NAME%%|$(git config user.name)|g" \
     -e "s|%%GIT_EMAIL%%|$(git config user.email)|g"
