7d7e1f03fbb20a3e06c30b7b285bc6e285ded54a
Antonio Braz <antonio@torreaobraz.com>
2026-01-02 11:34:23 -0600

    refactor(nvim): organize plugins into category subdirectories

---
+++ .config/nvim/lua/custom/plugins/ui/devicons.lua
@@ -0,0 +1,34 @@
+return {
+    "nvim-tree/nvim-web-devicons",
+    event = "VeryLazy",
+    opts = {
+        override_by_filename = {
+            ["dune"] = {
+                icon = "🐫",
+                name = "Dune",
+            },
+            ["dune-workspace"] = {
+                icon = "🐫",
+                name = "DuneWorkspace",
+            },
+            ["dune-project"] = {
+                icon = "🐫",
+                name = "DuneProject",
+            },
+            [".ocamlformat"] = {
+                icon = "🐫",
+                name = "OCamlFormat",
+            },
+        },
+        override_by_extension = {
+            opam = {
+                icon = "🐫",
+                name = "Opam",
+            },
+        },
+        strict = true,
+    },
+    config = function(_, opts)
+        require("nvim-web-devicons").setup(opts)
+    end,
+}
