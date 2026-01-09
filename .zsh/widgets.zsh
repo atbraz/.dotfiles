67b68fd8aa25be8761fe55ddad7f44d69159b2ba
Antonio Braz <antonio@torreaobraz.com>
2026-01-08 20:36:20 -0600

    feat(zsh): add Ctrl+Z toggle for suspend/resume

--- .zsh/widgets.zsh
+++ .zsh/widgets.zsh
@@ -1,5 +1,20 @@
 # Zsh Line Editor (ZLE) Widgets and Keybindings
 
+# Ctrl+Z toggle - suspend/resume foreground process
+# If command line is empty, brings last suspended job to foreground
+# If command line has content, suspends current input (push-input)
+function _ctrl_z_toggle() {
+    if [[ $#BUFFER -eq 0 ]]; then
+        BUFFER="fg"
+        zle accept-line
+    else
+        zle push-input
+        zle clear-screen
+    fi
+}
+zle -N _ctrl_z_toggle
+bindkey '^Z' _ctrl_z_toggle
+
 # Tmux sessionizer widget - bound to Ctrl+F
 function tmux_sessionizer_widget() {
     LBUFFER=""
