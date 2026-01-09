661304dcfb2a0e7bffc8d4c8e416181661664a08
Antonio Braz <antonio@torreaobraz.com>
2026-01-08 20:36:07 -0600

    feat(nvim): add oil.nvim file explorer

---
+++ .config/nvim/lua/custom/plugins/editor/oil.lua
@@ -0,0 +1,40 @@
+return {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    opts = {
+        default_file_explorer = false,
+        columns = { "icon" },
+        keymaps = {
+            ["g?"] = { "actions.show_help", mode = "n" },
+            ["<CR>"] = "actions.select",
+            ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+            ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+            ["<C-t>"] = { "actions.select", opts = { tab = true } },
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = { "actions.close", mode = "n" },
+            ["q"] = { "actions.close", mode = "n" },
+            ["<C-r>"] = "actions.refresh",
+            ["-"] = { "actions.parent", mode = "n" },
+            ["_"] = { "actions.open_cwd", mode = "n" },
+            ["`"] = { "actions.cd", mode = "n" },
+            ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+            ["gs"] = { "actions.change_sort", mode = "n" },
+            ["gx"] = "actions.open_external",
+            ["g."] = { "actions.toggle_hidden", mode = "n" },
+            ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        },
+        view_options = {
+            show_hidden = true,
+        },
+        float = {
+            padding = 2,
+            max_width = 100,
+            max_height = 30,
+        },
+    },
+    keys = {
+        { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+        { "<leader>-", "<cmd>Oil --float<cr>", desc = "Open Oil (float)" },
+    },
+}
