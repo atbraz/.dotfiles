33ce17c1d112a126fa0de35ee2e3d4052736f357
Antonio Braz <antonio@torreaobraz.com>
2026-01-11 00:00:04 -0600

    chore: auto-sync (weekly)

--- scripts/tmux-sessionizer.zsh
+++ scripts/tmux-sessionizer.zsh
@@ -38,6 +38,7 @@ get_project_paths() {
     add_single_path "$DOTFILES"
     add_single_path "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian Vault/"
     add_paths_from_dir "$CODE/" "$FD_CMD"
+    add_paths_from_dir "$CODE/Assignments/" "$FD_CMD"
 
     # Output: "display_name\tfull_path" for fzf to use
     local -a entries=()
